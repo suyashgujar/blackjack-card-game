0a52d4262e1a25fa5ea8aacc7043f87588195ef4
Suyash Gujar <suyashgujar007@gmail.com>
2026-04-13 12:56:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let firstCard = 3
+let firstCard = 6
 let secondCard = 9
 
 let sum = firstCard + secondCard
+
+if (sum < 21){
+    console.log("Do you want to draw a new card ??")
+}
+else if (sum === 21){
+    console.log("You got the BlackJack !! Congratulations !! ")
+}
+else{
+    console.log("Sorry, You are out of the game")
+}
