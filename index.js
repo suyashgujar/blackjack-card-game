33201377024d82ea2bb3a4da4766f047a91cd16a
Suyash Gujar <suyashgujar007@gmail.com>
2026-04-14 16:12:19 +0530

    sum display added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
 
 let sum = firstCard + secondCard
 
@@ -7,19 +7,26 @@ let hasBlackJack = false
 let isAlive = true
 
 let message = ""
+let messageEL = document.getElementById("message-el")
+let sumEL = document.getElementById("sum-el")
 
 function start(){
     
+    sumEL.textContent = "Sum: " + sum
+
+
     if (sum < 21){
     message = "Do you want to draw a new card ??"
-}
-else if (sum === 21){
-    message = "You got the BlackJack !! Congratulations !! "
-    hasBlackJack = true
-}
-else{
-    message = "Sorry, You are out of the game"
-    isAlive = false
-}
+    }
+    else if (sum === 21){
+        message = "You got the BlackJack !! Congratulations !! "
+        hasBlackJack = true
+    }
+    else{
+        message = "Sorry, You are out of the game"
+        isAlive = false
+    }
+
+    messageEL.textContent = message
 
 }
